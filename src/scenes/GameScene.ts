a4b87c2d6ecf9adb79342c2c05ae38dd322c22df
AndreyUA <infernalsteel666@gmail.com>
2024-03-09 12:21:45 +0200

    applied background as tileSprite and implemented side scrolling for background

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -7,6 +7,7 @@ import { Dragon } from "../prefabs/Dragon";
 export class GameScene extends Phaser.Scene {
   dragon: Dragon | null = null;
   cursors: Phaser.Types.Input.Keyboard.CursorKeys | null = null;
+  backgroundTileSprite: Phaser.GameObjects.TileSprite | null = null;
 
   constructor() {
     super({ key: SceneKeys.GAME, active: false });
@@ -29,10 +30,22 @@ export class GameScene extends Phaser.Scene {
 
   update(_time: number, _delta: number): void {
     this.dragon?.onMove();
+
+    if (this.backgroundTileSprite) {
+      this.backgroundTileSprite.tilePositionX += 0.6;
+    }
   }
 
   createBackground(): void {
-    this.add.sprite(0, 0, AssetKeys.MAIN_SCENE_BACKGROUND).setOrigin(0, 0);
+    this.backgroundTileSprite = this.add
+      .tileSprite(
+        0,
+        0,
+        +this.game.config.width,
+        +this.game.config.height,
+        AssetKeys.MAIN_SCENE_BACKGROUND
+      )
+      .setOrigin(0, 0);
   }
 
   createCursorKeys(): void {
